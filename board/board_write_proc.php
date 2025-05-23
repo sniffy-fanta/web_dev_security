4dd4887e01b690b3ab54e88b87bca5bc8b408db2
sniffy-fanta <minhyeok1864@naver.com>
2025-05-23 19:47:43 +0900

    add

--- board/board_write_proc.php
+++ board/board_write_proc.php
@@ -6,19 +6,24 @@
     $title = $_POST['title'];
     $content = $_POST['content'];
     $author = $_SESSION['user_id'];
-    $upload_file = $_FILES['upload_file'];
-    
-    //DB 데이터 저장
-    $sql = "INSERT INTO board (title,author,content,post_date,file,views,likes) VALUES ('$title','$author','$content',now(),0,0)";
-    $result = $mysqli->query($sql);
 
-    if($result){
-        echo "<script>
-                alert('게시글이 작성되었습니다.');
-                location.href='/board/board.php';
-            </script>";
-    }
-    else{
-        echo "관리자에게 문의하세요";
-    }
+    $filename = $_FILES['upload_file']['name'];
+    $tmp_filename = $_FILES['upload_file']['tmp_name'];
+    $file_dir = $_SERVER['DOCUMENT_ROOT']."/board/uploads/";
+    $file_path = $_SERVER['DOCUMENT_ROOT']."/board/uploads/".$filename;
+
+    if(move_uploaded_file($tmp_filename,$file_path)){}
+        //DB 데이터 저장
+        $sql = "INSERT INTO board (title,author,content,post_date,file,views,likes) VALUES ('$title','$author','$content',now(),$file_path,0,0)";
+        $result = $mysqli->query($sql);
+
+        if($result){
+            echo "<script>
+                    alert('게시글이 작성되었습니다.');
+                    location.href='/board/board.php';
+                </script>";
+        }
+        else{
+            echo "관리자에게 문의하세요";
+        }
 ?>
