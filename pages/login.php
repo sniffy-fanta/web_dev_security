bce25ee5028fbdc595e4a1a84ed8160b2812dafe
sniffy-fanta <minhyeok1864@naver.com>
2025-10-31 19:03:45 +0900

    인증 제한 기능 1차 보완

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    $msg = $_SESSION['login_error'] ?? '';
+    $locked = !empty($_SESSION['login_switch']);
+    unset($_SESSION['login_error']);
+?>
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -10,14 +16,24 @@
 <body>
     <div class="login_box">
         <h2>로그인</h2>
-        <form action="/proc/login_proc.php" method="POST">
-            <div class="input_field">
-                <input type="text" name="user_id" placeholder="아이디" autocomplete="off" required>
-                <input type="password" name="user_pw" placeholder="비밀번호" autocomplete="off" required>
-            </div>
-            <a href="/pages/register.php" id="register_link">회원가입</a>
-            <button type="submit">로그인</button>
-        </form>
+        <?php if ($msg): ?>
+            <div class="alert"><?= htmlspecialchars($msg, ENT_QUOTES, 'UTF-8') ?></div>
+        <?php endif; ?>
+
+        <?php if($locked): ?>
+            <!-- 잠금 로그인 뷰 -->
+            <p></p>
+        <?php else: ?>
+            <!-- 일반 로그인 폼 -->
+            <form action="/proc/login_proc.php" method="POST">
+                <div class="input_field">
+                    <input type="text" name="user_id" placeholder="아이디" autocomplete="off" required>
+                    <input type="password" name="user_pw" placeholder="비밀번호" autocomplete="off" required>
+                </div>
+                <a href="/pages/register.php" id="register_link">회원가입</a>
+                <button type="submit">로그인</button>
+            </form>
+        <?php endif; ?>
     </div>
 </body>
 </html>
--- proc/login_proc.php
+++ proc/login_proc.php
@@ -5,8 +5,26 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/php/db.php';
 //입력값 변수에 저장하기
 $user_id = $_POST['user_id'];
 $user_pw = $_POST['user_pw'];
-//입력받은 아이디의 비밀번호를 DB에서 조회
-$sql = "SELECT userpw FROM users WHERE userid= ?";
+
+//로그인 일반 실패 함수
+function common_failed_login($msg){
+    echo "<script>
+            alert('{$msg}');
+            history.back();
+        </script>";
+        exit;
+}
+
+//로그인 잠금 처리 함수
+function locked_login($msg){
+    $_SESSION['login_error'] = $msg;
+    $_SESSION['login_switch'] = 1;
+    header("Location:/pages/login.php");
+    exit;
+}
+
+//입력받은 아이디의 비밀번호 및 실패 카운트, 잠금 해제 시간 DB에서 조회
+$sql = "SELECT userpw, failed_cnt, lock_until FROM users WHERE userid= ?";
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("s", $user_id);
 $stmt->execute();
@@ -14,23 +32,65 @@ $result = $stmt->get_result();
 
 //$result 는 쿼리가 성공하기만 하면 true이기 때문에 결과가 0개여도 true를 반환
 //$result-> num_rows > 0 은 1개이상의 결과가 있어야 하는데 result가 false라면 실행조차 x
-if($result && $result->num_rows >0){
-    $row = $result->fetch_assoc();
+if(!$result || $result->num_rows === 0){
+    common_failed_login("아이디 또는 비밀번호가 틀렸습니다.");
+}
+$row = $result->fetch_assoc();
 
-    if($row['userpw']===$user_pw){
-        //DB비밀번호와 입력한 비밀번호가 같을 시 세션저장
-        session_regenerate_id(true);
-        $_SESSION['user_id'] = $user_id;
-        header("Location:/pages/main.php");
-        exit;
-    }
-    else{
-        header("Location:/proc/login_fail.php");
-        exit;
+//잠금 상태 확인
+if(!empty($row['lock_until'])){
+    $now = new DateTime('now', new DateTimeZone('UTC')); //현재시간
+    $lock_until = new DateTime($row['lock_until'], new DateTimeZone('UTC')); //잠금 해제 시간
+
+    echo $now->format('Y-m-d H:i:s');
+
+
+    if($lock_until > $now){
+        locked_login("잠금 된 상태입니다. 10분 후 다시 시도해 주세요.");
+    } else{
+        $update_sql = "UPDATE users SET failed_cnt = 0, lock_until = NULL WHERE userid = ?";
+        $upStmt = $mysqli -> prepare($update_sql);
+        $upStmt -> bind_param("s", $user_id);
+        $upStmt -> execute();
+
+        $row['failed_cnt'] = 0;
+        $row['lock_until'] = NULL;
     }
 }
-else{
-    header("Location:/proc/login_fail.php");
+
+//비밀번호 비교
+if($row['userpw'] === $user_pw) {
+    //로그인 성공 (실패 횟수 초기화)
+    $success_sql = "UPDATE users SET failed_cnt = 0, lock_until = NULL WHERE userid = ?";
+    $suStmt = $mysqli -> prepare($success_sql);
+    $suStmt -> bind_param("s", $user_id);
+    $suStmt -> execute();
+
+    session_regenerate_id(true);
+    $_SESSION['user_id'] = $user_id;
+    header("Location:/pages/main.php");
     exit;
+} else {
+    //로그인 실패
+    $failed_cnt = $row['failed_cnt'] + 1;
+
+    if($failed_cnt >= 5){
+        // 로그인 5회 이상 실패 (잠금)
+        $locked_sql = "UPDATE users SET failed_cnt = 0, lock_until = (UTC_TIMESTAMP() + INTERVAL 1 MINUTE) WHERE userid = ?";
+        $lockStmt = $mysqli -> prepare($locked_sql);
+        $lockStmt -> bind_param("s", $user_id);
+        $lockStmt -> execute();
+
+        locked_login("5회 이상 실패하여 10분간 로그인이 잠금되었습니다.");
+    }else {
+        //실패횟수 누적
+        $update2_sql = "UPDATE users SET failed_cnt = ? WHERE userid = ?";
+        $up2Stmt = $mysqli -> prepare($update2_sql);
+        $up2Stmt -> bind_param("is", $failed_cnt, $user_id);
+        $up2Stmt -> execute();
+
+        $count = 5 - $failed_cnt;
+        common_failed_login("로그인 실패, 남은 시도 횟수: {$count}회");
+    }
 }
 ?>
